4b73dfca4d975c5836a3604e866fe6dd883a2d95
reznychenkooryna <reznychenkooryna@gmail.com>
2025-10-24 23:40:12 +0300

    first master commit

---
+++ Lab_06_03_02/Lab_06_03_02.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+template <typename T>
+void SwapEvenOdd(T* a, const int size, int i = 0)
+{
+    if (i < size - 1)
+    {
+        T temp = a[i];
+        a[i] = a[i + 1];
+        a[i + 1] = temp;
+        SwapEvenOdd(a, size, i + 2);
+    }
+}
+
+template <typename T>
+void PrintArray(T* a, const int size, int i = 0)
+{
+    if (i < size)
+    {
+        cout << a[i] << " ";
+        PrintArray(a, size, i + 1);
+    }
+    else
+    {
+        cout << endl;
+    }
+}
+
+int main()
+{
+    const int n = 10;
+
+    int a[n] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    double b[n] = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.0 };
+    string c[n] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+
+    cout << "Before swap (int): ";
+    PrintArray(a, n);
+    SwapEvenOdd(a, n);
+    cout << "After swap (int): ";
+    PrintArray(a, n);
+    cout << endl;
+
+    cout << "Before swap (double): ";
+    PrintArray(b, n);
+    SwapEvenOdd(b, n);
+    cout << "After swap (double): ";
+    PrintArray(b, n);
+    cout << endl;
+
+    cout << "Before swap (string): ";
+    PrintArray(c, n);
+    SwapEvenOdd(c, n);
+    cout << "After swap (string): ";
+    PrintArray(c, n);
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			const int n = 6;
+			int a[n] = { 1, 2, 3, 4, 5, 6 };
+			int expected = 9;
+			int result = SumEvenIndices(a, n);
+			Assert::AreEqual(expected, result);
+		}
+	};
+}
